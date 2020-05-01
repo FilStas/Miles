eb623e6d3661f0d314ca2e4cadb7ee532fe36836
FilStas <filstas23@gmail.com>
2020-05-01 11:10:55 +0400

    add files project

---
+++ src/FreeMiles.java
@@ -0,0 +1,8 @@
+public class FreeMiles {
+    public static void main(String[] args) {
+        int bonusOneMiles = 20;
+        int ticketAmount = 3500;
+        int accruedBonuses = ticketAmount / bonusOneMiles;
+        System.out.println(accruedBonuses);
+    }
+}
